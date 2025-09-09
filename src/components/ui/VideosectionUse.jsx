66ae1e4044238f8cfe09ec8a2697208311b9c7e5
creation <creation2224@gmail.com>
2025-09-09 16:17:11 +0530

    video link fixed

--- src/components/ui/VideosectionUse.jsx
+++ src/components/ui/VideosectionUse.jsx
@@ -6,14 +6,14 @@ export function HeroVideoDialogDemoTopInBottomOut() {
       <HeroVideoDialog
         className="block dark:hidden"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/watch?v=hxHRVaobt5k"
+        videoSrc="https://www.youtube.com/embed/hxHRVaobt5k"
         thumbnailSrc="https://i.ibb.co/VYtyrGFp/Boost-Focus-With-Work-Ping-0a347c1b-aa40-4681-b765-2633dd5a334d.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/watch?v=hxHRVaobt5k"
+        videoSrc="https://www.youtube.com/embed/hxHRVaobt5k"
         thumbnailSrc="https://i.ibb.co/VYtyrGFp/Boost-Focus-With-Work-Ping-0a347c1b-aa40-4681-b765-2633dd5a334d.png"
         thumbnailAlt="Hero Video"
       />
